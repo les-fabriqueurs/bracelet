49633b84b2f1ff28084f605285f19e019afd65cd
François Jacob <papamaker.fr@gmail.com>
2017-07-15 17:58:32 +0200

    Update attinyWS2812b.ino

--- attinyWS2812b/attinyWS2812b.ino
+++ attinyWS2812b/attinyWS2812b.ino
@@ -1,2 +1,20 @@
+#include "FastLED.h"
+#define DATA_PIN 3
 
+// Define the array of leds
+CRGB led;
+
+void setup() { 
+	LEDS.addLeds<WS2812B,DATA_PIN,GRB>(&led,1);
+	LEDS.setBrightness(255);
+}
+
+void loop() { 
+	  static uint8_t hue = 0;
+		led = CHSV(hue++, 255, 255);
+		// Show the leds
+		FastLED.show(); 
+		// Wait a little bit before we loop around and do it again
+		delay(50);
+}
 
